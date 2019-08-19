cb572920e5a386bd96d68d36b7922d9339e3434b
24thsaint <rave.arevalo@kingsland.io>
2019-08-19 16:46:09 +0800

    refactor(profile): custom field change protobuf field name from 'key' to 'label'

--- makeProto.bat
+++ makeProto.bat
@@ -3,6 +3,10 @@
 echo Compiling Protocols
 echo NOTE: Make sure you have 'protoc.exe' extracted to your path as well
 echo          as having 'github.com/golang/protobuf' installed in go.
+echo (1) choco install protoc
+echo (2) go get -d -u github.com/golang/protobuf/protoc-gen-go
+echo (3) go install github.com/golang/protobuf/protoc-gen-go
+echo (4) Also, ensure that your GO/bin is added to path
 
 pushd %cd%
 cd pb/protos
